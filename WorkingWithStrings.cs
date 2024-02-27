bbf99679bf81f60f672a5330124bdeb4c0cfdf13
mikaa2001 <mikaa2001@gmail.com>
2024-02-28 00:09:59 +0500

    Working with strings

---
+++ WorkingWithStrings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WorkingWithStrings
+{
+	internal class WorkingWithStrings
+	{
+		static void Main(string[] args)
+		{
+			Console.Write("Как вас зовут? ");
+			string name = Console.ReadLine();
+			Console.Write("Сколько вам лет? ");
+			string age = Console.ReadLine();
+			Console.Write("Каков ваш знак зодиака? ");
+			string zodiacSign = Console.ReadLine();
+			Console.Write("Где вы работаете? ");
+			string placeOfWork = Console.ReadLine();
+
+			Console.WriteLine($"Вас зовут {name}, вам {age} год, вы {zodiacSign} и работаете {placeOfWork}");
+		}
+	}
+}
